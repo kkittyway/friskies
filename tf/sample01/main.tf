b2e6e49509253f993f0628246e480ae5ffe5d2eb
kkittyway <k.kittyway@gmail.com>
2020-09-13 02:01:46 -0700

    +

---
+++ tf/sample01/main.tf
@@ -0,0 +1,14 @@
+provider "azurerm" {
+  version = "=2.24.0"
+
+  subscription_id = "${file("../account.json")}"
+  client_id       = ""
+  client_secret   = ""
+  tenant_id       = ""
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "NAME_OF_THE_VM-rg"
+  location = "West Europe"
+}
